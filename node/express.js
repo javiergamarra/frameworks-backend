ca7a76a7bc4a550c8b53474b3fd307cea9d0ea29
nhpat <nhpatt@gmail.com>
2025-06-11 01:05:27 +0200

    1 - verbs

--- node/express.js
+++ node/express.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello, Express!');
+const students = [{name: "javier", surname: "gamarra"}, {name: "jorge", surname: "sanz"},]
+
+app.get('/students', (req, res) => {
+    res.send(students);
+});
+
+app.get('/students/0', (req, res) => {
+    res.send(students[0]);
+});
+
+app.delete('/students/0', (req, res) => {
+    students.splice(0, 1)
+    res.send(undefined)
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
