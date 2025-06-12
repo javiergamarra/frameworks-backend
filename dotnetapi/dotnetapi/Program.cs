6c3a72975e28520a06905bfe07961c4a847cb116
nhpat <nhpatt@gmail.com>
2025-06-12 03:26:03 +0200

    .NEt sample code

--- dotnetapi/dotnetapi/Program.cs
+++ dotnetapi/dotnetapi/Program.cs
@@ -1,10 +1,20 @@
 using System.Collections;
+using System.ComponentModel.DataAnnotations;
+using dotnetapi;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddProblemDetails();
+builder.Services.AddEndpointsApiExplorer(); 
+builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<IAlumnoService, AlumnoService>();
 
 var app = builder.Build();
 
@@ -14,16 +24,60 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
-app.UseHttpsRedirection();
 
+
+// Configure exception handling
+app.UseExceptionHandler(appBuilder =>
+{
+    appBuilder.Run(async context =>
+    {
+        var exceptionHandlerFeature = context.Features.Get<IExceptionHandlerFeature>();
+        var exception = exceptionHandlerFeature.Error;
+        
+        var problemDetails = new ProblemDetails
+        {
+            Status = StatusCodes.Status500InternalServerError,
+            Title = "An unexpected error occurred!",
+            Detail = exception.Message,
+            Instance = context.Request.Path
+        };
+        
+        // Log the exception
+        var logger = context.RequestServices.GetRequiredService<ILogger<Program>>();
+        logger.LogError(exception, "Unhandled exception");
+        
+        context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+        context.Response.ContentType = "application/problem+json";
+        
+        await context.Response.WriteAsJsonAsync(problemDetails);
+    });
+});
+
+
+
+app.UseHttpsRedirection();
+app.UseRouting();
+app.MapControllers();
+app.UseSwagger();
+app.UseSwaggerUI();
 
 
 app.MapGet("/students", () =>
     {
-        return new ArrayList { new Student("Javier", "Gamarra"),  new Student("Jorge", "Sanz") };;
+        var students = new ArrayList { new Student(0, "Javier", "Gamarra"),  new Student(1, "Jorge", "Sanz") };
+        return students;
     })
     .WithName("GetStudents");
 
 app.Run();
 
-public record Student(string name, string surname);
+public record Student(long id, [MinLength(3)] string name, string surname);
+
+public class AlumnoService : IAlumnoService
+
+{
+    public Student create(Student student)
+    {
+        return student;
+    }
+}
---
+++ dotnetapi/dotnetapi/StudentsController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnetapi;
+
+[ApiController]
+[Route("api/students")]
+public class StudentsController(IAlumnoService alumnoService) : ControllerBase
+{
+    private List<Student> _students = [new(0, "Javier", "Gamarra"), new(1, "Jorge", "Sanz")];
+
+    [HttpGet]
+    public ActionResult<IEnumerable<Student>> GetAll()
+    {
+        var requestHeader = Request.Headers["Authorization"];
+
+        throw new Exception();
+        _students = [new Student(0, "Javier", "Gamarra"), new Student(1, "Jorge", "Sanz")];
+        return _students;
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<Student> GetById(int id)
+    {
+        var student = _students.FirstOrDefault(p => p.id == id);
+        if (student == null)
+            return NotFound();
+
+        return student;
+    }
+    
+    
+    [HttpPost]
+    public ActionResult<Student> Create(Student student)
+    {
+        alumnoService.create(student);
+        return student;
+    }
+}
+
+public interface IAlumnoService
+{
+    Student create(Student student);
+}
